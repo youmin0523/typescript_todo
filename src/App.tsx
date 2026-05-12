7b1da33aee44526eee205d91f17ca76c3d7c09db
youminsu0523 <youminsu0523@gmail.com>
2026-05-12 14:27:24 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button';
+
+const App = () => {
+  return (
+    <div className="">
+      <Button className="rounded-sm">Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
